e6b7c483c9dca0b1aec23a41160e3ece6b883511
megawepic <efps10.5pyapdarren@gmail.com>
2025-12-16 19:52:33 +0800

    add blank.html and game.js

--- game.js
+++ game.js
@@ -17,37 +17,49 @@ function getHumanChoice(){
     return selection.toUpperCase()
 }
 
-
-
-function playRound(humanChoice, computerChoice) {
-    if((humanChoice == "STONE" && computerChoice == "paper") || (humanChoice == "SCISSORS" && computerChoice == 
-        "stone") || (humanChoice == "PAPER" && computerChoice == "scissors")){
-        computerScore ++
-        console.log(`CPU played ${computerChoice}. You lose. Computer gains a point`)
-    } else if ((humanChoice == "STONE" && computerChoice == "scissors") || (humanChoice == "SCISSORS" && computerChoice == "paper") || (humanChoice == "PAPER" && computerChoice == "stone")) {
-        humanScore ++
-        console.log(`CPU played ${computerChoice}. You win. You gain a point`)
-    } else{
-        console.log(`CPU played ${computerChoice}. Draw play again`)
-    }
-}
-
 let humanScore = 0
 let computerScore = 0
 
-function playGame() {
+const scissors = document.querySelector("#Scissors")
+const paper = document.querySelector("#Paper")
+const stone = document.querySelector("#Stone")
+
+scissors.addEventListener("click", function(){
+    playRound("SCISSORS", getComputerChoice())
+})
+paper.addEventListener("click", function(){
+    playRound("PAPER", getComputerChoice())
+})
+stone.addEventListener("click", function(){
+    playRound("STONE", getComputerChoice())
+})
 
 
-    for (let i = 0; i < 5; i++){
+function playRound(humanChoice, computerChoice) {
+    const score = document.getElementById("score")
 
-        const humanSelection = getHumanChoice()
-        const computerSelection = getComputerChoice()
-        playRound(humanSelection, computerSelection)
-
+    score.textContent += humanChoice
+    score.textContent += " " + computerChoice
+    
+    
+    if((humanChoice == "STONE" && computerChoice == "paper") || (humanChoice == "SCISSORS" && computerChoice == 
+        "stone") || (humanChoice == "PAPER" && computerChoice == "scissors")){
+        computerScore ++
+        score.textContent = `CPU played ${computerChoice}. You lose. Computer gains a point. Current score is ${humanScore} - ${computerScore}`
+    } else if ((humanChoice == "STONE" && computerChoice == "scissors") || (humanChoice == "SCISSORS" && computerChoice == "paper") || (humanChoice == "PAPER" && computerChoice == "stone")) {
+        humanScore ++
+        score.textContent = `CPU played ${computerChoice}. You win. You gain a point. Current score is ${humanScore} - ${computerScore}`
+    } else{
+        score.textContent = `CPU played ${computerChoice}. Draw play again. Current score is ${humanScore} - ${computerScore}`
     }
-
-    const results =  `You score ${humanScore} and the cpu score ${computerScore}`
-    humanScore = 0
-    computerScore = 0
-    return results
+    
+    if(humanScore == 5){
+        score.textContent = "You have won. LFFGGGG. Continue to play another game"
+        computerScore = 0
+        humanScore = 0
+    } else if (computerScore == 5){
+        score.textContent = "CPU won this round. Get your revenge. Continue to play another game"
+        computerScore = 0
+        humanScore = 0
+    }
 }
